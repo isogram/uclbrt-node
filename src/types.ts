c23a4ccce780927aa665d03def02856948fe6997
Muhammad Shidiq <emshidiq@gmail.com>
2023-09-10 22:57:18 +0700

    initial commit

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+// interface for UserConfig object
+export interface UserConfig {
+    accountSid: string;
+    authToken: string;
+    apiHost: string;
+    cardHost: string;
+    debug: boolean;
+};
